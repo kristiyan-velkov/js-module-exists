996584b0aa4a3aeb556f89f782dd8f11bf4e11f6
Kristiyan Velkov <kristiyan.velkov@gfk.com>
2022-08-12 17:56:49 +0300

    [fix] Readme file imports

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,4 @@
 #!/usr/bin/env node
-"use strict";
-import { moduleExists } from "../scr/js/module-exists.js";
-import {
-  moduleExistsWithText,
-  setTextColors,
-} from "../scr/js/module-exists-with-text.js";
-
 export { moduleExists } from "../scr/js/module-exists.js";
 export {
   moduleExistsWithText,
