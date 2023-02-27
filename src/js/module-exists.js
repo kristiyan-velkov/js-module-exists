514f6146266d0647b6743fcbd06cda723dba9d22
Christiyan <christiyanweb@gmail.com>
2023-02-27 17:03:22 +0200

    [fix] Problem with find packages start with @

--- src/js/module-exists.js
+++ src/js/module-exists.js
@@ -4,16 +4,24 @@ import fs from "fs";
 
 export const moduleExists = (moduleName) => {
   const packageFilePath = path.dirname("");
-  const data = fs.readFileSync(`${packageFilePath}/package.json`, "utf8");
-  let result;
+  const data = readFileSync(`${packageFilePath}/package.json`, "utf8");
+  let dependenciesList = [];
 
-  if (data.includes(moduleName)) {
-    result = true;
-  } else {
-    result = false;
+  if (data) {
+    let packageJson = JSON.parse(data);
+
+    if (packageJson.dependencies && dependencies) {
+      dependenciesList.push(...Object.keys(packageJson.dependencies));
+    }
+    if (packageJson.devDependencies && devDependencies) {
+      dependenciesList.push(...Object.keys(packageJson.devDependencies));
+    }
+    if (packageJson.peerDependencies && peerDependencies) {
+      dependenciesList.push(...Object.keys(packageJson.peerDependencies));
+    }
   }
 
-  return result;
+  return dependenciesList.includes(packageName);
 };
 
 export default moduleExists;
